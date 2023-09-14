bf0196ebcb6fce378f39ecd8a310e002e7e1dda4
MDTabrez24 <142489611+MDTabrez24@users.noreply.github.com>
2023-09-14 15:49:43 +0530

    Add files via upload

---
+++ mahak.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+int a=30;
+int b=a%2;
+int c=a/2;
+int d=c%2;
+printf("%d",d);
+int e=c/2;
+int f=e%2;
+printf("%d",f);
+int g=e/2;
+int h=g%2;
+printf("%d",h);
+int i=g/2;
+int j=i%2;
+printf("%d",j);
+printf("%d",b);
+}
